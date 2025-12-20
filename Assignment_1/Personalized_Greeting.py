306ebb577cf8b7669e5465d1bacc0f003445e8f4
Supal Choudhary <Supal.Choudhary@rackspace.com>
2025-12-20 21:09:58 +0530

    Added files for mathematical operation and personalized greeting.

---
+++ Assignment_1/Personalized_Greeting.py
@@ -0,0 +1,11 @@
+"""
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+# Answer:
+f_name = input("Enter your first name: ")
+l_name = input("Enter your last name: ")
+print('Hello,', f_name, l_name + "!", 'Welcome to Python program.')
